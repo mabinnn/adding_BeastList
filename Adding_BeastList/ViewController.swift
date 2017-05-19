36ba88f62afd6561e70066701d0068b249cf89a4
Marvin Alganes <marvin.alganes@gmail.com>
2017-05-19 15:29:12 -0700

    initial commit

--- Adding_BeastList/ViewController.swift
+++ Adding_BeastList/ViewController.swift
@@ -8,18 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    var tasks: [String] = [String]()
+    
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    // When button is pressed, we are adding new items in the array and it should display on the screen by the tableView.reloadData()
+    
+    @IBAction func beastButton(_ sender: UIButton) {
+        if sender.tag == 1{
+            tasks.append(taskTextField.text!)
+            print("Pressing Beast Button")
+            print(tasks)
+            tableView.reloadData()
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource{
+    
+    // This function returns how many items are in the tasks array: numbersOfRows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    
+    // This function creates the array items and place into a cell, cellForRowAt
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+    
+    
+    // function to remove items from the array
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Section: \(indexPath.section) and row \(indexPath.row) are selected!")
+        tasks.remove(at: indexPath.row)
+        tableView.reloadData()
+    }
+    
+    
+    
+    
+    
+    
 }
 
